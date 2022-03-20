d588e9d0292afa4a887f430b87a78e07ed449fa9
shikha735 <shikha735@gmail.com>
2022-03-20 16:03:36 -0600

    Edit data list updated. Employee files added

--- php-files/edit-data-list.php
+++ php-files/edit-data-list.php
@@ -21,33 +21,36 @@
     	<li><a href="login.php">Logout</a></li> 
       </ul>
     </div>
-  </nav>
+</nav>
 <div id="container">
+	<br><br>
+	<h1 align="center">Edit Data</h1>
     <ul class="grid">
       <li>
         <p>Add a team to UAtheletics Department</p>
         <a href="add-team.php" class="red-btn">Add Team</a>
       </li>
       <li>
-        <p>Update a team to UAtheletics Department</p>
-        <a href="team-update.php" class="red-btn">Update Team</a>
-      </li>
-      <li>
         <p>Get teams list of UAtheletics Department</p>
         <a href="teams.php" class="red-btn">Get Teams List</a>
       </li>
-      <li>
+	  <li>
         <p>Add an event in UAtheletics Department</p>
-        <a href="add-event.php" class="red-btn">Add event</a>
-      </li>
-      <li>
-        <p>Update an event in UAtheletics Department</p>
-        <a href="update-event.php" class="red-btn">Update Event</a>
+        <a href="add-event.php" class="red-btn">Add Event</a>
       </li>
       <li>
         <p>Get events list in UAthletics Department</p>
         <a href="events.php" class="red-btn">Get Events List</a>
       </li>
-      </div>
+      <li>
+        <p>Add an employee to UAtheletics Department</p>
+        <a href="add-employee.php" class="red-btn">Add Employee</a>
+      </li>
+      <li>
+        <p>Get the list of employees in UAtheletics Department</p>
+        <a href="employees.php" class="red-btn">Get Employee List</a>
+      </li>
+      
+</div>
 </body>
 </html>
