e365d647be9ca4ad4de467008d78697c14610451
Ritesh Yejju <yejju.ritesh@gmail.com>
2022-03-20 21:04:34 -0600

    Update events.php

--- php-files/events.php
+++ php-files/events.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+if($_SESSION['role']=='user' or $_SESSION['role']=='admin'){
 
 require_once  'db.php';
 
@@ -24,7 +26,7 @@ if($conn->connect_error) die($conn->connect_error);
     	<li><a href="home.php">Home</a></li>
 		<li><a href="teams.php">Teams</a></li>
 		<li><a href="events.php">Events</a></li>
-    	<li><a href="login.php">Logout</a></li> 
+    	<li><a href="logout.php">Logout</a></li> 
       </ul>
     </div>
   </nav>
@@ -60,3 +62,8 @@ if($conn->connect_error) die($conn->connect_error);
 	</div>
 </body>
 </html>
+<?php
+}else{
+echo 'Un-Authorized';
+}
+?>
