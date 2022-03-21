647e6abf03cf2796774f7f1f0eccb43c12b3758c
Ritesh Yejju <yejju.ritesh@gmail.com>
2022-03-20 20:35:07 -0600

    Update reports-list.php

--- php-files/reports-list.php
+++ php-files/reports-list.php
@@ -1,9 +1,11 @@
-
+<?php
+session_start();
+if($_SESSION['role']=='user' or $_SESSION['role']=='admin'){
+?>
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="../css-files/list.css">
-<link rel="stylesheet" type="text/css" href="../css-files/team-update.css">
+<link rel="stylesheet" type="text/css" href="../css-files/home.css">
 <link href="https://fonts.googleapis.com/css?family=Arima+Madurai" rel="stylesheet">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 </head>
@@ -18,16 +20,26 @@
 		<li><a href="home.php">Home</a></li>
 		<li><a href="teams.php">Teams</a></li>
 		<li><a href="events.php">Events</a></li>
-    	<li><a href="login.php">Logout</a></li> 
+    	<li><a href="logout.php">Logout</a></li> 
       </ul>
     </div>
   </nav>
-<div id="container">
-    <ul class="grid">
-      <li>
-        <p>Team Vs Profits</p>
-        <a href="teamsVsProfits.php" class="red-btn">Check Team Profits</a>
-      </li>
-      </div>
+<div class="center">
+<div class="container">
+  <p class="title">Reports</p>
+  <div class="overlay"></div>
+  <div class="button"><a href="reports-list.php"> Click Here </a></div>
+</div>
+<div class="container">
+   <p class="title">Edit data</p>
+  <div class="overlay"></div>
+  <div class="button"><a href="edit-data-list.php"> Click Here </a></div>
+</div>
+</div>
 </body>
 </html>
+<?php
+}else{
+echo 'Un-Authorized';
+}
+?>
