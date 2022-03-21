f086986cd2c65cefbe8eaa466aff90255b8b0de0
Ritesh Yejju <yejju.ritesh@gmail.com>
2022-03-20 22:43:25 -0600

    Update login.php

--- php-files/login.php
+++ php-files/login.php
@@ -1,5 +1,14 @@
 <?php
 
+function mysql_entities_fix_string($conn, $string){
+	return htmlentities(mysql_fix_string($conn, $string));	
+}
+
+function mysql_fix_string($conn, $string){
+	$string = stripslashes($string);
+	return $conn->real_escape_string($string);
+}
+
 require_once 'db.php';
 
 $conn = new mysqli($hn, $un, $pw, $db);
