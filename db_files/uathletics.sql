2467ea3231b463f6561c977e2b897cf36b8556d7
Ritesh Yejju <yejju.ritesh@gmail.com>
2022-03-20 21:56:29 -0600

    trasactions data dump, employee, credential tables

--- db_files/uathletics.sql
+++ db_files/uathletics.sql
@@ -134,9 +134,65 @@ CREATE TABLE `transaction_ledger` (
 INSERT INTO `transaction_ledger` (`income_id`, `team_id`, `transaction_amount`, `transaction_type`) VALUES
 (1, 1, 15000, 'income'),
 (5, 7, 13010, 'income'),
-(2, 1, 1300, 'expense'),
+(16, 20, 1000, 'income'),
+(2, 1, -1300, 'expense'),
 (3, 2, 12900, 'income'),
+(6, 15, 12900, 'income'),
+(7, 16, 1290, 'income'),
+(8, 17, -300, 'expense'),
+(9, 18, -2300, 'expense'),
+(10, 19, -1700, 'expense'),
+(11, 17, 12900, 'income'),
+(12, 18, 12900, 'income'),
+(13, 19, 12900, 'income'),
+(14, 8, 100, 'income'),
+(15, 9, 900, 'income'),
+(17, 9, -100, 'expense'),
+(18, 15, -9000, 'expense'),
 (4, 2, 1400, 'income');
+
+
+--
+-- Table structure for table `employee`
+--
+create table `employee` (
+employee_id INTEGER not null,
+first_name varchar(255),
+last_name varchar(255),
+title varchar(255),
+address varchar(255),
+start_date date,
+end_date date,
+salary_type varchar(255),
+Urole varchar(255),
+primary key (employee_id),
+unique(first_name, last_name)
+)
+
+--
+-- Table structure for table `credentials`
+--
+CREATE TABLE `credentials` (
+	username varchar(255),
+	Upassword varchar(255),
+	employee_id INTEGER,
+	Foreign Key(employee_id) REFERENCES employee(employee_id)
+)
+
+--
+-- Dumping data for table `employee`
+--
+insert into employee (employee_id, first_name, last_name, title, address, start_date, end_date, salary_type, Urole) values (1, 'Paul', 'Jones', 'Coach', '6035 Buena Vista Crossing', '5/23/2021', null, 'employee', 'user');
+insert into employee (employee_id, first_name, last_name, title, address, start_date, end_date, salary_type, Urole) values (2, 'Brad', 'Smith', 'IT Admin', '07 Badeau Park', '8/29/2020', null, 'employee', 'admin');
+insert into employee (employee_id, first_name, last_name, title, address, start_date, end_date, salary_type, Urole) values (3, 'Johnath', 'Fidoe', 'Staff ', '9874 La Follette Circle', '8/13/2021', '2/2/2024', 'contractor', 'user');
+
+--
+-- Dumping data for table `credentials`
+--
+insert into credentials (username, Upassword, employee_id) values ('pjones','$2y$10$HPQIAR6W0XFtu3NL4r2k4ukaWLQEn/pCTypDFRGADABYMg.K2fHb6',1);
+insert into credentials (username, Upassword, employee_id) values ('bsmith','$2y$10$9eESNSeNm0FhMWDiS/3RS.ua2otDa7sd43S27y4Zb6yjPGkuxQRIK',2);
+insert into credentials (username, Upassword, employee_id) values ('jfidoe','$2y$10$HPQIAR6W0XFtu3NL4r2k4ukaWLQEn/pCTypDFRGADABYMg.K2fHb6',3);
+
 
 --
 -- Indexes for dumped tables
